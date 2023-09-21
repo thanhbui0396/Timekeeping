edd82272f320cfac4b1c4f0e01fbcf345fd64735
thanh bui (strigram) <thanhbui96@odoo.com>
2023-09-21 23:05:37 +0700

    uipdate

--- timekeeping_app/android/app/src/main/kotlin/com/example/timekeeping_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.timekeeping_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- timekeeping_app/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:timekeeping_app/my_app.dart';
-
-void main(List<String> args) {
-  runApp(MyApp());
-}
